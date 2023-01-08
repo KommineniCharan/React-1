3543e331e4a8504865d189eecdc768ce9cd351a9
KommineniCharan <charank347@gmail.com>
2023-01-08 10:11:03 +0530

    okie

---
+++ react-1/src/Message.js
@@ -0,0 +1,9 @@
+function Message() {
+  return (
+    <div>
+      <h2>Welcome to Message.js</h2>
+    </div>
+  );
+}
+
+export default Message;
---
+++ react-1/src/index.js
@@ -0,0 +1,14 @@
+//import React from "react";
+import ReactDOM from "react-dom/client";
+import React from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <App />
+  </div>
+);
+
+//React.render("Good Evening", document.getElementById("root"));
